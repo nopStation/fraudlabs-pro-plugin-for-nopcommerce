5bf34f2d15fcc9b6d7425684fddbf15938a975e6
Jaber Kibria <mhsjaber@gmail.com>
2021-07-10 02:06:24 +0600

    INIT PLUGIN

---
+++ Nop.Plugin.Misc.FraudLabsPro/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Nop.Core.Infrastructure.DependencyManagement;
+using Nop.Plugin.Misc.FraudLabsPro.Factories;
+using Nop.Plugin.Misc.FraudLabsPro.Services;
+
+namespace Nop.Plugin.Misc.FraudLabsPro.Infrastructure
+{
+    /// <summary>
+    /// Represents a FraudLabs Pro dependency registrar
+    /// </summary>
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        /// <summary>
+        /// Register services and interfaces
+        /// </summary>
+        /// <param name="services">Collection of service descriptors</param>
+        /// <param name="typeFinder">Type finder</param>
+        /// <param name="appSettings">App settings</param>
+        public void Register(IServiceCollection services, ITypeFinder typeFinder, AppSettings appSettings)
+        {
+            //register custom services
+            services.AddScoped<FraudLabsProManager>();
+
+            //register custom factories
+            services.AddScoped<FraudLabsProOrderModelFactory>();
+        }
+
+        /// <summary>
+        /// Order of this dependency registrar implementation
+        /// </summary>
+        public int Order => 2;
+    }
+}
